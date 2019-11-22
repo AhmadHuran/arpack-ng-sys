ba099fbbdef7f6f4641891e777e1d532ff978f42
Daniel Hauck <mail@dhauck.eu>
2019-11-22 13:19:31 +0100

    Build and generate bindings.

---
+++ build.rs
@@ -0,0 +1,18 @@
+use bindgen::Builder;
+use cmake::Config;
+use std::{env, path::PathBuf};
+
+fn main() {
+    let dst = Config::new("source").define("ICB", "ON").build();
+    let bindings = Builder::default()
+        .header(&format!("{}/include/arpack/arpack.h", dst.display()))
+        .generate()
+        .expect("Could not generate bindings.");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Could not write bindings!");
+
+    println!("cargo:rustc-link-search=native={}/build", dst.display());
+    println!("cargo:rustc-link-lib=static=arpack");
+}
