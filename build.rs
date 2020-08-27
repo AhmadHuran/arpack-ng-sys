5529908b898f89907ab82b20f40040991eee5e1d
Daniel Hauck <mail@dhauck.eu>
2020-08-27 15:09:54 +0200

    Add option to link to system library.

--- build.rs
+++ build.rs
@@ -1,24 +1,39 @@
 use bindgen::Builder;
-use cmake::Config;
 use std::{env, path::PathBuf};
 
 fn main() {
-    let dst = Config::new("source")
-        .define("ICB", "ON")
-        .define("CMAKE_Fortran_FLAGS", "-fPIC")
-        .build();
-    let bindings = Builder::default()
-        .header(&format!("{}/include/arpack/arpack.h", dst.display()))
-        .generate()
-        .expect("Could not generate bindings.");
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Could not write bindings!");
+    #[cfg(feature = "static")]
+    {
+        let dst = cmake::Config::new("source")
+            .define("ICB", "ON")
+            .define("BUILD_TESTING", "OFF")
+            .define("CMAKE_Fortran_FLAGS", "-fPIC")
+            .build();
+        let bindings = Builder::default()
+            .header(&format!("{}/include/arpack/arpack.h", dst.display()))
+            .generate()
+            .expect("Could not generate bindings.");
+        let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+        bindings
+            .write_to_file(out_path.join("bindings.rs"))
+            .expect("Could not write bindings!");
 
-    println!("cargo:rustc-link-search=native={}/build", dst.display());
-    println!("cargo:rustc-link-lib=static=arpack");
-    println!("cargo:rustc-link-lib=gfortran");
-    println!("cargo:rustc-link-lib=blas");
-    println!("cargo:rustc-link-lib=lapack");
+        println!("cargo:rustc-link-search=native={}/build", dst.display());
+        println!("cargo:rustc-link-lib=static=arpack");
+        println!("cargo:rustc-link-lib=gfortran");
+        println!("cargo:rustc-link-lib=blas");
+        println!("cargo:rustc-link-lib=lapack");
+    }
+    #[cfg(feature = "system")]
+    {
+        let library = pkg_config::probe_library("arpack").expect("Could not find arpack");
+        let bindings = Builder::default()
+            .header("arpack-sys.h")
+            .generate()
+            .expect("Could not generate bindings.");
+        let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+        bindings
+            .write_to_file(out_path.join("bindings.rs"))
+            .expect("Could not write bindings!");
+    }
 }
